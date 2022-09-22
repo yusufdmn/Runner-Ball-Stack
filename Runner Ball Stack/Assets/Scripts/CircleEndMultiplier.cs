3b3e2240a93ea6a5b0d874e0b63cdaaa967ff425
Yusuf Duman <yusufdumans61@gmail.com>
2022-09-22 21:26:56 +0300

    little fixes

---
+++ Runner Ball Stack/Assets/Scripts/CircleEndMultiplier.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleEndMultiplier : ScoreMultiplier
+{
+    [SerializeField] CircleEnd circleEnd;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ball" || other.tag == "FirstBall")
+        {
+            other.tag = "PassedLine";
+            if (!circleEnd.areCirclesFull)
+                StartCoroutine(circleEnd.FlyNextBallToCircle());
+            else
+                StartCoroutine(circleEnd.FlyNextBallToLastSpot());
+            circleEnd.areCirclesFull = circleEnd.CheckIfCirclesFull();
+
+        }
+    }
+}
