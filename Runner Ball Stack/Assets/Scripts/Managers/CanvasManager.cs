8deb30eca0e04561c861fccd1440eae8328d4936
Yusuf Duman <yusufdumans61@gmail.com>
2022-12-31 17:18:35 +0300

    Update CanvasManager.cs

--- Runner Ball Stack/Assets/Scripts/Managers/CanvasManager.cs
+++ Runner Ball Stack/Assets/Scripts/Managers/CanvasManager.cs
@@ -58,45 +58,4 @@ public class CanvasManager : MonoBehaviour
         settingsPopUp.SetActive(!settingsPopUp.activeSelf);
     }
 
-
-    /*[SerializeField] GameObject mainMenu;
-    [SerializeField] GameObject hidedMenu;
-    [SerializeField] GameObject endPanel;
-    [SerializeField] GameObject failPanel;
-    [SerializeField] GameObject extraIncomePanel;
-    [SerializeField] GameObject unlockObstaclePanel;
-    [SerializeField] GameObject settingsPopUp;
-    
-    public void HideMainMenu()
-    {
-        hidedMenu.SetActive(false);
-    }
-    public void DisplayEndPanel()
-    {
-        StartCoroutine(DisableExtraPanel());
-        endPanel.SetActive(true);
-        mainMenu.SetActive(false);
-    }
-    public void DisplayFailPanel()
-    {
-        failPanel.SetActive(true);
-        mainMenu.SetActive(false);
-    }
-
-    IEnumerator DisableExtraPanel()
-    {
-        yield return new WaitForSeconds(1.5f);
-        extraIncomePanel.SetActive(false);
-    }
-
-    public void DisplayUnlockObstaclePanel()
-    {
-        unlockObstaclePanel.SetActive(true);
-    }
-
-    public void DisplaySettingsPopUp()
-    {
-        settingsPopUp.SetActive(!settingsPopUp.activeSelf);
-    }
-    */
 }
